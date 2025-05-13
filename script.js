3e3d662a33c350e3fc8023fcf2df3000849f0096
Krishnareddy-1807 <krishnareddyyaramala24@gmail.com>
2025-05-13 13:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const romanMap = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
-  //your code here
-	let n = 798;
-	let ans=""
-	while(n!=0){
-		for(let i in obj){
-			let value = obj[i][0];
-			let num = obj[i][1]
-			if(num<=n){
-				ans=ans+value;
-				n=n-num;
-				break;
-			}
-		}
-	}
+  let result = "";
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
+}
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
-
-
+// console.log(convertToRoman(36));
 
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
